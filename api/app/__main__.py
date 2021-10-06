c0eb1d1fe28784bc5b1883c09c072866d26558fa
Asdrubalini <indirizzoemail@mail.com>
2021-10-06 21:51:08 +0200

    add api

--- api/app/__main__.py
+++ api/app/__main__.py
@@ -1,18 +1,37 @@
-from flask import Flask
-from database import insert_reading
+from flask import Flask, request
+from flask.wrappers import Response
+from database import insert_reading, fetch_readings
+
+import json
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["GET"])
 def index():
-    return "ciao"
+    readings = fetch_readings()
+
+    html = ""
+    for reading in readings:
+        html += f"<pre>{reading}</pre>"
+    
+    return html
 
 
-@app.route("/insert", methods=["GET"])
-def insert():
-    insert_reading(1, 22.34)
+@app.route("/insert", methods=["POST", "GET"])
+def insert_reading():
+    data = request.json
+
+    if not data or ("sensor_id" not in data or "temperature" not in data):
+        return Response("Missing required parameters", status=400)
+
+    sensor_id = data["sensor_id"]
+    temperature = data["temperature"]
+
+    insert_reading(sensor_id, temperature)
+
+    return "ok"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8001)
+    app.run(host="0.0.0.0", port=8001, debug=True)
--- api/app/database.py
+++ api/app/database.py
@@ -17,7 +17,32 @@ __database = get_database()
 def insert_reading(sensor_id: int, temperature: float):
     cursor = __database.cursor()
 
-    cursor.execute(
-        '''INSERT INTO `readings` (`reading_id`, `sensor_id`, `temperature`, `created_at`) '''
-        '''VALUES(NULL, %s, %s, current_timestamp())''', (sensor_id, temperature)
-    )
+    cursor.execute('''
+        INSERT INTO `readings` (`reading_id`, `sensor_id`, `temperature`, `created_at`) 
+        VALUES(NULL, %s, %s, current_timestamp())
+    ''', (sensor_id, temperature))
+
+    cursor.close()
+    __database.commit()
+
+
+def fetch_readings():
+    cursor = __database.cursor()
+
+    cursor.execute('''
+        SELECT
+            `sensors`.`name`,
+            `sensors`.`description`,
+            `readings`.`temperature`,
+            `readings`.`created_at`
+        FROM
+            `readings`
+        JOIN `sensors` ON `readings`.`sensor_id` = `sensors`.`sensor_id`;
+    ''')
+
+    data = cursor.fetchall()
+
+    cursor.close()
+    __database.commit()
+
+    return data
